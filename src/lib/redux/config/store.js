af8408c2222e0be3d82b8c627d7b6bdb976a8c3e
Wahyu Koco <74239131+Kakaranara@users.noreply.github.com>
2023-10-19 14:12:21 +0700

    redux-storage setup

--- src/lib/redux/config/store.js
+++ src/lib/redux/config/store.js
@@ -1,8 +1,21 @@
 import { combinedReducer } from './../reducers'
 import { configureStore } from '@reduxjs/toolkit'
-// const { configureStore } = require("@reduxjs/toolkit");
+import persistReducer from 'redux-persist/es/persistReducer'
+import persistStore from 'redux-persist/es/persistStore'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, combinedReducer)
+
+// export const store = configureStore({
+//     reducer: combinedReducer,
+// })
 export const store = configureStore({
-    reducer: combinedReducer,
+    reducer : persistedReducer,
 })
 
+export const persistor = persistStore(store)
